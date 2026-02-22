ed8c542cec069199da8959a937a284ddac70b66e
ahmed-hazem-1 <ahmed453189@fci.bu.edu.eg>
2026-02-22 02:05:21 +0200

    configure serverless handler to support binary file types

--- functions/api.js
+++ functions/api.js
@@ -1,4 +1,6 @@
 const serverless = require('serverless-http');
 const app = require('../src/app');
 
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app, {
+  binary: ['image/*', 'application/pdf', 'multipart/form-data', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'text/plain']
+});
